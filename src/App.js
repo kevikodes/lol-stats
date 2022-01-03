9840c47a72595513dfb7f1d8acf8dd88245b961d
Kevin Owens III <krowens3rd@gmail.com>
2022-01-02 20:16:05 -0800

    Project setup

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+function App() {
+  return <Container></Container>;
+}
+
+export default App;
+
+const Container = styled.div`
+  height: 100vh;
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+`;
